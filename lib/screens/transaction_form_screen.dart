8ebf92478b9cc55b0282c21402a43ff4f4667074
IsraelMerlyn <merlyn142a@gmail.com>
2025-05-10 23:38:41 -0600

    tasks #2.4 Agregando datos en el formulario

--- lib/screens/transaction_form_screen.dart
+++ lib/screens/transaction_form_screen.dart
@@ -1,5 +1,8 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gastos_app/models/transaction.dart';
+import 'package:gastos_app/providers/transaction_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:uuid/uuid.dart';
 
 class TransactionFormScreen extends StatefulWidget {
   const TransactionFormScreen({super.key});
@@ -14,6 +17,8 @@ class _TransactionFormScreenState extends State<TransactionFormScreen> {
   final TextEditingController _descriptionController = TextEditingController();
 
   String _selectedCategory = 'Comida';
+  TransactionType _selectedType = TransactionType.expense;
+
   final List<String> _categories = [
     'Comida',
     'Transporte',
@@ -30,83 +35,123 @@ class _TransactionFormScreenState extends State<TransactionFormScreen> {
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
-        child: Form(child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextFormField(
-              controller: _amountController,
-              keyboardType: TextInputType.number,
-              validator: (value){
-                if (value == null || value.isEmpty) {
-                  return 'Por favor ingrese un monto';
-                }
-                return null;
-              },
-              decoration:  const InputDecoration(
-                labelText: 'Monto',
-                prefixIcon: Icon(Icons.attach_money),
-
-              ),
-            ),
-            const SizedBox(height:20.0),
-            TextFormField(
-              controller: _descriptionController,
-
-              validator: (value){
-                if (value == null || value.isEmpty) {
-                  return 'Por favor ingrese una descripcion';
-                }
-                return null;
-              },
-              decoration:  const InputDecoration(
-                labelText: 'Descripcion',
-                prefixIcon: Icon(Icons.description_rounded),
-
-              ),
-            ),
-            const SizedBox(height:20.0),
-
-            DropdownButtonFormField<String>(
-              value: _selectedCategory,
-              items: _categories.map((String category) {
-                return DropdownMenuItem<String>(
-                  value: category,
-                  child: Text(category),
-                );
-              }).toList(),
-              onChanged: (String? newValue) {
-                setState(() {
-                  _selectedCategory = newValue!;
-                });
-              },
-              decoration: const InputDecoration(
-                labelText: 'Categoría',
-                prefixIcon: Icon(Icons.category),
-              ),
-            ),
-            const SizedBox(height:20.0),
-            Center(
-              child: ElevatedButton(onPressed: () {
-                if(_formKey.currentState!.validate()){
-                  // Aquí puedes agregar la lógica para guardar la transacción
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Transacción guardada')),
-                  );
-                  Navigator.pop(context);
-                }
-              },
-              child: const Text('Guardar Transacción'),
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Monto',
+                  prefixIcon: Icon(Icons.attach_money),
                 ),
+                controller: _amountController,
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor ingrese un monto';
+                  }
+                  return null;
+                },
               ),
-            )
-          ],
-        ),
+              const SizedBox(height: 20.0),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Descripcion',
+                  prefixIcon: Icon(Icons.description_rounded),
+                ),
+                controller: _descriptionController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor ingrese una descripcion';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20.0),
+              DropdownButtonFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Categoría',
+                  prefixIcon: Icon(Icons.category),
+                ),
+                value: _selectedCategory,
+                items: _categories.map((category) {
+                  return DropdownMenuItem(
+                    value: category,
+                    child: Text(category),
+                  );
+                }).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    _selectedCategory = value!;
+                  });
+                },
+              ),
+              const SizedBox(height: 20.0),
+              Row(
+                children: [
+                  Expanded(
+                    child: RadioListTile(
+                      title: const Text('Gastos'),
+                      value: TransactionType.expense,
+                      groupValue: _selectedType,
+                      onChanged: (TransactionType? value) {
+                        setState(() {
+                          // Aquí puedes manejar el cambio de tipo de transacción
+                          _selectedType = value!;
+                        });
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    child: RadioListTile(
+                      title: const Text('Ingresos'),
+                      value: TransactionType.income,
+                      groupValue: _selectedType,
+                      onChanged: (TransactionType? value) {
+                        setState(() {
+                          // Aquí puedes manejar el cambio de tipo de transacción
+                          _selectedType = value!;
+                        });
+                      },
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(height: 20.0),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      // Aquí puedes agregar la lógica para guardar la transacción
+                      final newTransaction = Transaction(
+                          id: const Uuid().v4(),
+                          category: _selectedCategory,
+                          amount: double.parse(_amountController.text),
+                          type: _selectedType,
+                          date: DateTime.now());
 
+                      Provider.of<TransactionProvider>(context, listen: false)
+                          .addTransaction(newTransaction);
+
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Transacción guardada')),
+                      );
+                      Navigator.pop(context);
+                    }
+                  },
+                  child: const Text('Guardar Transacción'),
+                  // style: ElevatedButton.styleFrom(
+                  //   padding: const EdgeInsets.symmetric(
+                  //       horizontal: 20, vertical: 15),
+                  //   shape: RoundedRectangleBorder(
+                  //     borderRadius: BorderRadius.circular(12),
+                  //   ),
+                  // ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
