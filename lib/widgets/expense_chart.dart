249a547f5a3be180db5175f09b2830e4a89a1134
IsraelMerlyn <merlyn142a@gmail.com>
2025-05-10 23:53:50 -0600

    tasks #2.5 Agregando provider a las graficas

--- lib/widgets/expense_chart.dart
+++ lib/widgets/expense_chart.dart
@@ -1,5 +1,8 @@
 import 'package:charts_flutter/flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:gastos_app/models/transaction.dart';
+import 'package:gastos_app/providers/transaction_provider.dart';
+import 'package:provider/provider.dart';
 
 import '../models/expense_data.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
@@ -10,18 +13,30 @@ class ExpenseChart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<charts.Series<ExpenseData , String>> series = [
+    final transactionProvider = Provider.of<TransactionProvider>(context);
+    final transactions = transactionProvider.transactions;
+
+    final exponses = transactions
+        .where((transaction) => transaction.type == TransactionType.expense)
+        .toList();
+    final Map<String, double> categoryTotals = {};
+
+    for (var transaction in exponses) {
+      categoryTotals[transaction.category] =
+          (categoryTotals[transaction.category] ?? 0) + transaction.amount;
+    }
+
+    List<charts.Series<MapEntry<String, double>, String>> series = [
       charts.Series(
-        id: 'Expenses',
-        data: expenseData,
-        domainFn: (ExpenseData data, _) => data.category,
-        measureFn: (ExpenseData data, _) => data.amomount,
-        colorFn: (_,__ ) =>
-            charts.MaterialPalette.blue.shadeDefault,
+        id: 'Gastos',
+        data: categoryTotals.entries.toList(),
+        domainFn: (MapEntry<String, double> entry, _) => entry.key,
+        measureFn: (MapEntry<String, double> entry, _) => entry.value,
+        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
       )
     ];
-    return  Container(
-      padding: EdgeInsets.all(20.0),
+    return Container(
+      padding: const EdgeInsets.all(20.0),
       height: 300,
       child: charts.BarChart(
         series,
